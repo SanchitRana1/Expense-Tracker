0c8c8eb87abfcacfe9e01833040a8b70f5c8fd2b
Rana <sanchitrana128@gmail.com>
2024-08-07 19:01:08 +0530

    url correction

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.VOTE_NODE_ENV === "production" ? "https://expense-tracker-api-ugtq.onrender.com" :"http://localhost:5000/api/v1"
+const BASE_URL = import.meta.VITE_NODE_ENV === "production" ? "https://expense-tracker-api-ugtq.onrender.com" :"http://localhost:5000/api/v1"
 export const INCOME_URL = `${BASE_URL}/income`;
 export const EXPENSE_URL = `${BASE_URL}/expense`;
 export const USER_URL = `${BASE_URL}/user`;
